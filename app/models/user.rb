9a4e023087e7450030672aad21f40d3a598495b3
xi <social@xiyiw.anonaddy.me>
2022-11-23 11:28:58 +0400

    Allow user to login using their email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,48 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # we sure to add case insensitivity to your validations on :username
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
+  # Create a login virtual attribute in the User model
+  # Add login as an User
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  # check if the same email as the username already exists in the database
+  def validate_username
+    errors.add(:username, :invalid) if User.where(email: username).exists?
+  end
+
+  # Because we want to change the behavior of the login action,
+  # we have to overwrite the find_for_database_authentication method.
+  # The method's stack works like this: find_for_database_authentication
+  # calls find_for_authentication which
+  # calls find_first_by_auth_conditions.
+  # Overriding the find_for_database_authentication method
+  # allows you to edit database authentication;
+  # overriding find_for_authentication
+  # allows you to redefine authentication at a specific point
+  # (such as token, LDAP or database).
+  # Finally, if you override the find_first_by_auth_conditions method,
+  # you can customize finder methods
+  # (such as authentication, account unlocking or password recovery).
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+
+    # if you want email to be case insensitive
+    conditions[:email].downcase! if conditions[:email]
+    where(conditions.to_h).first
+
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
