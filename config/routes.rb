73f8e98f7a30f52c3cca9ff3b09529cb43949079
xiyini <q2z@hotmail.com>
2022-11-26 18:22:44 +0400

    Add User index styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  # TODO: user_route_url
   devise_for :users
   root 'articles#index'
+  resources :users, only: %i[index show]
   resources :articles
 end
