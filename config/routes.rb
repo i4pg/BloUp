d6e062d52e4bdbf0d7958476322fad89f4a379b7
xiyini <q2z@hotmail.com>
2022-12-01 18:12:15 +0400

    Add routes reources for friendship

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: %i[index show]
-  resources :friend_requests, except: %i[new edit]
+  resources :friendships, except: %i[new edit]
   resources :articles do
     resources :likes
   end
