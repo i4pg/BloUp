b0f4f25e5f78eedf03bb621b455e7177d104bef4
xiyini <q2z@hotmail.com>
2022-12-03 14:35:59 +0400

    Implement a Real-Time WebSocket for the article's like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,10 @@ class Like < ApplicationRecord
   validates :user_id, uniqueness: { scope: :article_id }
   belongs_to :user
   belongs_to :article
+  after_commit :likes_counter
+
+  def likes_counter
+    counter = article.likes.count
+    broadcast_update_to(article, target: article, html: counter)
+  end
 end
