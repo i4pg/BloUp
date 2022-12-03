319abf50c87a290f2ad3e67323e26f7093903dc6
xiyini <q2z@hotmail.com>
2022-12-03 13:20:13 +0400

    Add like's system test

--- test/system/likes_test.rb
+++ test/system/likes_test.rb
@@ -2,23 +2,32 @@ require 'application_system_test_case'
 
 class LikesTest < ApplicationSystemTestCase
   setup do
+    # fixture has one like first user first article
     @user1 = users(:one)
     @user2 = users(:two)
-    @user3 = users(:three)
-
     @article1 = articles(:one)
     @article2 = articles(:two)
-    @article3 = articles(:three)
-
-    login_as users(:two)
   end
 
-  test 'when like button clicked add a like to article and increment the counter on the page' do
+  test "Increment article's like counter if user first click on like" do
+    login_as @user2
     visit articles_url
 
     assert_difference('@article2.likes.count', +1) do
-      click_on 'Like', match: :first
+      click_on 'Likes', match: :first
     end
-    assert_selector 'like', text: @article2.likes.count
+    assert_selector 'span', text: @article2.likes.count, id: "article_#{@article2.id}"
+  end
+
+  test "decrement article's like counter if user already like an article" do
+    # fixture has one like first user first article
+    # each test run sprit
+    login_as @user1
+    visit articles_url
+
+    assert_difference('@article1.likes.count', -1) do
+      click_on 'Likes', match: :first
+    end
+    assert_selector 'span', text: @article1.likes.count, id: "article_#{@article1.id}"
   end
 end
